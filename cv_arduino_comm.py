9274630c40a020764c9b95c009f21ede9c433a02
Cindy Li <cindyli0213@hotmail.com>
2020-02-16 00:34:07 -0500

    Update motor control

--- cv_arduino_comm.py
+++ cv_arduino_comm.py
@@ -1,36 +1,47 @@
 import serial
 import cv2
+from verify import predict
 
-SAVE_FILE = 'saved_image.jpg'
+SAVE_FILE = 'verifier/saved_image.jpg'
 ARDUINO_PORT = '/dev/ttyACM0'
 
 
 def video_cap(arduino):
     cap = cv2.VideoCapture(0)
+    cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('frame', 2000, 1500)
+
+    letter = ' '
 
     while True:
 
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
-        cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('frame', 2000, 2000)
 
-        h, w, _ = frame.shapes
-        h, w = int(h/2), int(w/2)
-        x, y = w - int(w/2), h - int(h/2)
+        roi_scale = 2
+        h, w, _ = frame.shape
+        roi_size = int(h/roi_scale)
+        x, y = int(w/2) - int(roi_size/2), int(h/2) - int(roi_size/2)
+
+        img = cv2.rectangle(frame, (x, y), (x + roi_size, y + roi_size), (0, 255, 0), 1)
+        cv2.putText(img, letter, (x, y - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow('Video Stream', img)
 
         if cv2.waitKey(1) & 0xFF == 27:
             break
 
-        elif cv2.waitKey(1) & 0xFF == ord('s'):
-            roi = frame[y:y+h, x:x+w]
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            roi = frame[y:y + roi_size, x:x + roi_size]
             cv2.imwrite(SAVE_FILE, roi)
 
-            # get letter based on image
-            # send_data(arduino, letter)
+            letter = predict()
 
-        img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
-        cv2.imshow('frame', img)
+            if letter == 'nothing':
+                send_data(arduino, '[')
+            elif letter == 'space':
+                send_data(arduino, '\\')
+            else:
+                send_data(arduino, letter)
 
     cap.release()
     cv2.destroyAllWindows()
